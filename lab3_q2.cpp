0c4469e7e0590d1018fdc1039b4bca8e87f1da47
tejasrokade25 <42237519+tejasrokade25@users.noreply.github.com>
2018-08-17 11:55:52 +0530

    Add files via upload

--- lab3_q2.cpp
+++ lab3_q2.cpp
@@ -1,15 +1,23 @@
-//This is the library
 #include<iostream>
 using namespace std;
 
-int main (){
+int main(){
 
-int c,d,e,f;
-cout<<"c=5+9"<<"\n";
-cout<<"d=5+[-9]"<<"\n";
-cout<<"e=5*9"<<"\n";
-cout<<"f=9%5"<<"\n";
-cout<<"g=5/9"<<"\n";
+int a=7;
+int b=5;
+int sum =a+b;
+int product =a*b;
+int difference =a-b;
+int quotient =a/b;	
+
+cout<<"a is 7"<<endl;
+cout<<"b is 5"<<endl;
+
+cout<<"Their sum is"<<sum<<endl;
+cout<<"Their product is "<<product<<endl;
+cout<<"Their difference is "<<difference<<endl;
+cout<<"a divided by b is "<<quotient<<endl;
+
 return 0;
 }
 

0406c60eaf8ac1cc2b8ea88699ac673885f34785
tejasrokade25 <42237519+tejasrokade25@users.noreply.github.com>
2018-08-17 10:49:58 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+float b;
+bool d;
+long int c;
+char k;
+
+cout<<"the size of a is"<<sizeof(a)<<"\n";
+cout<<"the size of b is "<<sizeof (b)<<"\n";
+cout<<"the size of d is "<<sizeof (d)<<"\n";
+cout<<"the size of c is "<<sizeof (c)<<"\n";
+cout<<"the size of k is "<<sizeof (k)<<"\n";
+return 0;
+}
+

d51c30ff8f9a8d2d49b2beb5f4572d6a1352ae5e
tejasrokade25 <42237519+tejasrokade25@users.noreply.github.com>
2018-08-17 11:23:18 +0530

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,15 @@
+//This is the library
+#include<iostream>
+using namespace std;
+
+int main (){
+
+int c,d,e,f;
+cout<<"c=5+9"<<"\n";
+cout<<"d=5+[-9]"<<"\n";
+cout<<"e=5*9"<<"\n";
+cout<<"f=9%5"<<"\n";
+cout<<"g=5/9"<<"\n";
+return 0;
+}
+
